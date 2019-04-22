a16bd1c4a5ddf597e14f188dacc8f979b1cdc558
Michael Gerver <mgerver@ufl.edu>
2019-04-22 01:26:21 -0400

    Javascript code added (array, object & while loop)

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+var members = [
+  'Dave Grohl',
+  'Nate Mendel',
+  'Pat Smear',
+  'Taylor Hawkins',
+  'Rami Jaffee',
+  'Chris Shiflett',
+];
+
+var tour = {
+  chicago: 'July 29 and 30, 2018',
+  seattle: 'September 1, 2018',
+  edmonton: 'September 4, 2018',
+};
+
+var i = 0;
+while (i < 5) {
+  console.log('Times running through the code: ' + i);
+
+  i++;
+}
